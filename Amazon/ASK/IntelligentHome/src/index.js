4041426cdc34e15582bc1c6b00c013b7792786f7
tsmitro <tsmitro@gmail.com>
2016-07-04 12:20:55 -0400

    Implemented index.js
    Implemented standard Alexa Skill session functionality such as welcome
    and cancel; added callbacks for custom Intents which currently do
    nothing other than output that the command was received.

---
+++ Amazon/ASK/IntelligentHome/src/index.js
@@ -0,0 +1,182 @@
+/**
+ * Main Lambda function script for handling interaction with the Intelligent Home skill.
+ */
+ 
+'use strict';
+
+// Route the incoming request based on type (LaunchRequest, IntentRequest,
+// etc.) The JSON body of the request is provided in the event parameter.
+exports.handler = function (event, context) {
+    try {
+        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+
+        /**
+         * Uncomment this if statement and populate with your skill's application ID to
+         * prevent someone else from configuring a skill that sends requests to this function.
+         */
+
+//     if (event.session.application.applicationId !== "amzn1.echo-sdk-ams.app.05aecccb3-1461-48fb-a008-822ddrt6b516") {
+//         context.fail("Invalid Application ID");
+//      }
+
+        if (event.session.new) {
+            onSessionStarted({requestId: event.request.requestId}, event.session);
+        }
+
+        if (event.request.type === "LaunchRequest") {
+            onLaunch(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "IntentRequest") {
+            onIntent(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "SessionEndedRequest") {
+            onSessionEnded(event.request, event.session);
+            context.succeed();
+        }
+    } catch (e) {
+        context.fail("Exception: " + e);
+    }
+};
+
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    // add any session init logic here
+}
+
+/**
+ * Called when the user invokes the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    console.log("onLaunch requestId=" + launchRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    getWelcomeResponse(callback);
+}
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+    console.log("onIntent requestId=" + intentRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    var intent = intentRequest.intent,
+        intentName = intentRequest.intent.name;
+
+    // handle yes/no intent after the user has been prompted
+    if (session.attributes && session.attributes.userPromptedToContinue) {
+        delete session.attributes.userPromptedToContinue;
+        if ("AMAZON.NoIntent" === intentName) {
+            handleFinishSessionRequest(intent, session, callback);
+        }
+    }
+
+    // dispatch custom intents to handlers here
+    if ("ChangeModelIntent" === intentName) {
+        handleChangeModelRequest(intent, session, callback);
+    } else if ("RotateCameraIntent" === intentName) {
+        handleRotateCameraRequest(intent, session, callback);
+    } else if ("AMAZON.HelpIntent" === intentName) {
+        handleGetHelpRequest(intent, session, callback);
+    } else if ("AMAZON.StopIntent" === intentName) {
+        handleFinishSessionRequest(intent, session, callback);
+    } else if ("AMAZON.CancelIntent" === intentName) {
+        handleFinishSessionRequest(intent, session, callback);
+    } else {
+        throw "Invalid intent";
+    }
+}
+
+/**
+ * Called when the user ends the session.
+ * Is not called when the skill returns shouldEndSession=true.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    // Add any cleanup logic here
+}
+
+// ------- Skill specific business logic -------
+
+function getWelcomeResponse(callback) {
+    var speechOutput = "Welcome to the intelligent home, how can I assist you?";
+    callback({}, buildSpeechletResponseWithoutCard(speechOutput, false));
+}
+
+function handleChangeModelRequest(intent, session, callback) {
+	// TODO: parse response and post to Adafruit RESTful interace.
+	
+    var speechOutput = "Model changed to " + intent.slots.Model.value + ".";
+	callback({}, buildSpeechletResponseWithoutCard(speechOutput, false));
+}
+
+function handleRotateCameraRequest(intent, session, callback) {
+	// TODO: parse response and post to Adafruit RESTful interace.
+	
+    var speechOutput = "Camera rotated " + intent.slots.Direction.value + ".";
+	callback({}, buildSpeechletResponseWithoutCard(speechOutput, false));
+}
+
+function handleGetHelpRequest(intent, session, callback) {
+    // Provide a help prompt for the user, explaining how the game is played. Then, continue the game
+    // if there is one in progress, or provide the option to start another one.
+
+    // Set a flag to track that we're in the Help state.
+    session.attributes.userPromptedToContinue = true;
+    var speechOutput = "I can interact with smart devices or manipulate the virtual environment. What would you like to do?";
+    callback(session.attributes, buildSpeechletResponseWithoutCard(speechOutput, false));
+}
+
+function handleFinishSessionRequest(intent, session, callback) {
+    // End the session with a "Good bye!" if the user wants to quit the game
+    callback(session.attributes,
+        buildSpeechletResponseWithoutCard("Good bye!", "", true));
+}
+
+// ------- Helper functions to build responses -------
+
+function buildSpeechletResponse(title, output, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        card: {
+            type: "Simple",
+            title: title,
+            content: output
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildSpeechletResponseWithoutCard(output, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: "1.0",
+        sessionAttributes: sessionAttributes,
+        response: speechletResponse
+    };
+}
