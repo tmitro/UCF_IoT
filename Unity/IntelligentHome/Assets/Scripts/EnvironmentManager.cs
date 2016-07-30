7533b3ac603c83bcf25dc00763c6a8fa4e6e3ac2
tsmitro <tsmitro@gmail.com>
2016-07-29 21:53:52 -0400

    Added test code to Unity MQTT client

--- Unity/IntelligentHome/Assets/Scripts/EnvironmentManager.cs
+++ Unity/IntelligentHome/Assets/Scripts/EnvironmentManager.cs
@@ -6,20 +6,35 @@ using System;
 
 public class EnvironmentManager : MonoBehaviour
 {
-    private MqttClient _mqttClient;
+    /// <summary>
+    /// A collection of swapable models.
+    /// </summary>
     public List<GameObject> _models;
 
+    private MqttClient _mqttClient;
+    private GameObject _activeModel;
+    private IDictionary<string, GameObject> _modelsByName;
+    private bool _rotatingCamera;
+    private RotationAxis _rotationAxis;
+    private float _rotationAngle;
+    private float _transitionTime = 2.0f;
+    private float _elapsedTransitionTime;
+
     /// <summary>
-    /// Used  for initialization.
+    /// Used for initialization.
     /// </summary>
     public void Start()
     {
+        _modelsByName = new Dictionary<string, GameObject>();
+
         // Initialize models in scene.
         foreach (var model in _models)
         {
+            _modelsByName[model.name] = model;
             model.SetActive(false);
         }
-        _models.FirstOrDefault().SetActive(true);
+        _activeModel = _models.FirstOrDefault();
+        _activeModel.SetActive(true);
 
         // Create MQTT client.
         _mqttClient = new MqttClient();
@@ -32,7 +47,83 @@ public class EnvironmentManager : MonoBehaviour
     /// </summary>
     public void Update()
     {
+        // TODO: remove local test stubs.
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            SwapModel("Cube");
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            SwapModel("Sphere");
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            RotateCamera(CameraDirection.Right);
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            RotateCamera(CameraDirection.Right);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            RotateCamera(CameraDirection.Left);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            RotateCamera(CameraDirection.Right);
+        }
+        if (Input.GetKeyUp(KeyCode.Keypad0))
+        {
+            RotateCamera(CameraDirection.OneEighty);
+        }
 
+        if (_rotatingCamera)
+        {
+            float xRotation = 0f;
+            float zRotation = 0f;
+
+            //Camera.main.transform.LookAt(_activeModel.transform);
+
+            if (_rotationAxis == RotationAxis.Vertical)
+            {
+                xRotation = Input.GetAxis("Vertical") * _rotationAngle;
+            }
+            else
+            {
+                zRotation = Input.GetAxis("Horizontal") * _rotationAngle;
+            }
+
+            float step = _transitionTime * Time.deltaTime;
+            float orbitCircumfrance = 2F * rDistance * Mathf.PI;
+            float distanceDegrees = (_transitionTime / orbitCircumfrance) * 360;
+            float distanceRadians = (_transitionTime / orbitCircumfrance) * 2 * Mathf.PI;
+
+            if (_rotationAngle > 0)
+            {
+                transform.RotateAround(_activeModel.transform.position, Vector3.up, -rotationAmount);
+                _rotationAngle -= rotationAmount;
+            }
+            else if (_rotationAngle < 0)
+            {
+                transform.RotateAround(_activeModel.transform.position, Vector3.up, rotationAmount);
+                _rotationAngle += rotationAmount;
+            }
+
+
+            Camera.main.transform.eulerAngles = Vector3.Lerp(Camera.main.transform.eulerAngles, 
+                new Vector3(xRotation, 0, zRotation), Time.deltaTime * _transitionTime);
+
+            //Quaternion rotation = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+            //transform.rotation = Quaternion.Slerp(
+            //    Camera.main.transform.rotation, rotation, Time.time * _transitionTime);
+            _elapsedTransitionTime += Time.deltaTime;
+
+            if (_elapsedTransitionTime >= _transitionTime)
+            {
+                _elapsedTransitionTime = 0f;
+                _rotatingCamera = false;
+            }
+        }
     }
 
     /// <summary>
@@ -70,11 +161,43 @@ public class EnvironmentManager : MonoBehaviour
 
     private void SwapModel(string model)
     {
-
+        _activeModel.SetActive(false);
+        _activeModel = _modelsByName[model];
+        _activeModel.SetActive(true);
     }
 
-    private void RotateCamera(string direction)
+    private void RotateCamera(CameraDirection direction)
     {
-    
+        switch (direction)
+        {
+            case CameraDirection.Up:
+                _rotationAxis = RotationAxis.Vertical;
+                _rotationAngle = 90f;
+                break;
+            case CameraDirection.Down:
+                _rotationAxis = RotationAxis.Vertical;
+                _rotationAngle = -90f;
+                break;
+            case CameraDirection.Left:
+                _rotationAxis = RotationAxis.Horizontal;
+                _rotationAngle = -90f;
+                break;
+            case CameraDirection.Right:
+                _rotationAxis = RotationAxis.Horizontal;
+                _rotationAngle = 90f;
+                break;
+            case CameraDirection.OneEighty:
+                _rotationAxis = RotationAxis.Horizontal;
+                _rotationAngle = 180f;
+                break;
+        }
+
+        _rotatingCamera = true;
+    }
+
+    private enum RotationAxis
+    {
+        Horizontal,
+        Vertical
     }
 }
